d4152ba72fd556e3ccbae60031067ab882e2a856
Youssif <abdulyoussif@gmail.com>
2013-07-01 12:39:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoussifsBlog::Application.config.secret_token = '1f8b16cace3951f0664e9f81e86f32b9765ee9b94b0c102fc9d5bd1a8754aa07796a33a740ba5202250bfd9d9f7ba8760fb1083b39b01c1252f4ff543b953f40'
